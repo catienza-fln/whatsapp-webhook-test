ac69030b6317538dbe1ae04fe4640190cc485156
Carmelo Atienza <catienza@freelancer.com>
2025-10-02 17:56:20 +0800

    test out with challenge

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
   if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
-    res.status(200).send(challenge);
+    res.status(200).send({"challenge": challenge});
   } else {
     res.status(403).end();
   }
